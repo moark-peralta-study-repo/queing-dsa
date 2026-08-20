86a6317240ddbe77f9ab11b63c7992bd10c2cb6a
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-08-21 02:42:27 +0800

    feat: Staff model

---
+++ src/main/java/org/hospitalqueing/model/Staff.java
@@ -0,0 +1,61 @@
+package org.hospitalqueing.model;
+
+public class Staff {
+  private int staffId;
+  private int userId;
+
+  private String firstName;
+  private String lastName;
+
+  private int departmentId;
+
+  public Staff() {}
+
+  public Staff(int staffId, int userId, String firstName, String lastName, int departmentId) {
+    this.staffId = staffId;
+    this.userId = userId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.departmentId = departmentId;
+  }
+
+  public int getStaffId() {
+    return staffId;
+  }
+
+  public void setStaffId(int staffId) {
+    this.staffId = staffId;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getDepartmentId() {
+    return departmentId;
+  }
+
+  public void setDepartmentId(int departmentId) {
+    this.departmentId = departmentId;
+  }
+}
