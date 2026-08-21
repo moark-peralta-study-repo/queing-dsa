7d15b56a88826d829db59015a19d1f49b59f4c17
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-08-21 16:25:58 +0800

    feat: PatientService

---
+++ src/main/java/org/hospitalqueing/service/PatientService.java
@@ -0,0 +1,34 @@
+package org.hospitalqueing.service;
+
+import java.util.List;
+
+import org.hospitalqueing.dao.PatientDAO;
+import org.hospitalqueing.model.Patient;
+
+public class PatientService {
+  PatientDAO patientDAO;
+
+  PatientService(PatientDAO patientDAO) {
+    this.patientDAO = patientDAO;
+  }
+
+  public void createPatient(Patient patient) {
+    patientDAO.save(patient);
+  }
+
+  public Patient getPatientById(int patientId) {
+    return patientDAO.findById(patientId);
+  }
+
+  public List<Patient> getAllPatients() {
+    return patientDAO.findAll();
+  }
+
+  public void updatePatient(Patient patient) {
+    patientDAO.update(patient);
+  }
+
+  public void deletePatient(int patientId) {
+    patientDAO.delete(patientId);
+  }
+}
