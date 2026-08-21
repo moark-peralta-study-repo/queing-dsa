6ad0d65eb917bef704788ebdce445c58a3806c95
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-08-21 18:29:56 +0800

    fix: package declaration

--- src/main/java/org/hospitalqueing/service/QueueService.java
+++ src/main/java/org/hospitalqueing/service/QueueService.java
@@ -1,7 +1,8 @@
-package org.hospitalqueing.dao;
+package org.hospitalqueing.service;
 
 import java.util.List;
 
+import org.hospitalqueing.dao.QueueEntryDAO;
 import org.hospitalqueing.model.QueueEntry;
 
 public class QueueService {
